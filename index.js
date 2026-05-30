40f9083e9f4c67c0523dd3c2e3c4bac5aaa108eb
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-05-30 17:12:49 +0600

    implemented jwt web token to secure data on backend server

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 const uri = process.env.MONGO_URI;
 
 const PORT = process.env.PORT;
@@ -20,6 +21,27 @@ const client = new MongoClient(uri, {
   }
 });
 
+const JWKS = createRemoteJWKSet(
+  new URL("http://localhost:3000/api/auth/jwks")
+)
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({message: "Unauthorized"})
+  }
+  
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log(payload);
+     next()
+  } catch (error) {
+    return res.status(403).json({message: "Forbidden"})
+ }
+ 
+}
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -49,7 +71,7 @@ async function run() {
         res.send(result);
     });
 
-    app.get("/cars/:id", async (req, res) => {
+    app.get("/cars/:id",verifyToken, async (req, res) => {
       const { id } = req.params;
       const result = await carsCollection.findOne({ _id: new ObjectId(id) });
       res.json(result)
@@ -62,8 +84,43 @@ async function run() {
       res.json(result)
     })
 
-    app.get("/bookings", async (req, res) => {
+    app.get("/bookings", async (req, res) => {     
       const result = await bookingsCollection.find().toArray();
+      res.json(result)
+    })
+
+    app.get("/bookings/:userId", async (req, res) => {
+      const userId = req.params;
+      const result = await bookingsCollection.find({userId: userId.userId}).toArray();
+      res.json(result)
+    })
+
+    app.delete("/bookings/:bookingId", async (req, res) => {
+      const { bookingId } = req.params;
+      const result = await bookingsCollection.deleteOne({_id: new ObjectId(bookingId)  });
+      res.json(result)
+    })
+
+    app.get('/cars/car/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  const result = await carsCollection.find({ userId: userId }).toArray();
+  res.json(result);
+    })
+    // delete My car
+    app.delete('/cars/:id', async (req, res) => {
+  const {id} = req.params;
+  const result = await carsCollection.deleteOne({_id: new ObjectId(id)});
+  res.json(result);
+    })
+    
+    // edit my car
+    app.patch("/cars/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedData = req.body;
+      const result=  carsCollection.updateOne(
+        { _id: new ObjectId(id) },
+        {$set: updatedData}
+      )
       res.json(result)
     })
 
